0b429b937f95234bd00fb001084077856f577442
nljor <nljor@JONSSONS>
2017-02-28 22:20:09 +0100

    Add project files.

---
+++ do-while loops/do-while.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+
+	const string password = "hello";
+	string input;
+
+	do {
+		cout << "Enter your password > " << flush;
+		cin >> input;
+
+		if (input != password) {
+			cout << "Access denied" << endl;
+		}
+
+	} while (input != password);
+
+
+	cout << "Password accepted" << endl;
+
+
+	return 0;
+}
+
+
